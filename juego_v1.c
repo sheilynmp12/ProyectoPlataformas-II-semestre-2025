1f05a041fa42907a2bf2126838ff2cc7546ea04a
sheilynmp12 <sheilyn.morris@ucr.ac.cr>
2025-11-21 00:10:55 -0600

    v03 parpadeo de objetos y evitar colision

--- juego_v1.c
+++ juego_v1.c
@@ -52,18 +52,20 @@ void mover(Juego *j, SDL_Keycode key) {
 
 int colision(Posicion a, Posicion b) { return a.x == b.x && a.y == b.y; }
 
-void verificar(Juego *j) {
-    if (colision(j->jugador, j->moneda)) {
+void verificar(Juego *j, int colisionActiva) {
+    if (colisionActiva && colision(j->jugador, j->moneda)) {
         j->puntaje++;
         j->moneda.x = rand() % (WIDTH / TILE);
         j->moneda.y = rand() % (HEIGHT / TILE);
     }
-    for (int i = 0; i < NUM_OBS; i++) {
-        if (colision(j->jugador, j->obstaculos[i])) {
-            j->vidas--;
-            j->jugador.x = WIDTH / (2 * TILE);
-            j->jugador.y = HEIGHT / (2 * TILE);
-            break;
+    if (colisionActiva) {
+        for (int i = 0; i < NUM_OBS; i++) {
+            if (colision(j->jugador, j->obstaculos[i])) {
+                j->vidas--;
+                j->jugador.x = WIDTH / (2 * TILE);
+                j->jugador.y = HEIGHT / (2 * TILE);
+                break;
+            }
         }
     }
 }
@@ -78,15 +80,26 @@ void render_text(SDL_Renderer *ren, TTF_Font *font, const char *text, int x, int
 }
 
 int main() {
-    SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
-
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Error inicializando SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+    if (TTF_Init() != 0) {
+        printf("Error inicializando SDL_ttf: %s\n", TTF_GetError());
+        return 1;
+    }
     SDL_Window *win = SDL_CreateWindow("Atrapa la Moneda",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WIDTH, HEIGHT, 0);
+    if (!win) {
+        printf("Error creando ventana: %s\n", SDL_GetError());
+        return 1;
+    }
     SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-
-    // Font
+    if (!ren) {
+        printf("Error creando renderer: %s\n", SDL_GetError());
+        return 1;
+    }
     TTF_Font *font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20);
     if (!font) {
         printf("Error cargando fuente: %s\n", TTF_GetError());
@@ -98,7 +111,8 @@ int main() {
 
     int running = 1;
     SDL_Event e;
-    Uint32 lastTime = 0;
+    Uint32 lastTime = 0, lastToggle = SDL_GetTicks();
+    int mostrarObstaculos = 1;
 
     while (running && j.vidas > 0) {
         while (SDL_PollEvent(&e)) {
@@ -109,7 +123,14 @@ int main() {
             }
         }
 
-        verificar(&j);
+        Uint32 now = SDL_GetTicks();
+        // alternar visibilidad 
+        if (now - lastToggle > 1000) {
+            mostrarObstaculos = !mostrarObstaculos;
+            lastToggle = now;
+        }
+
+        verificar(&j, 1); // colisiones activas 
 
         // Renderizado
         SDL_SetRenderDrawColor(ren, 20, 20, 20, 255);
@@ -121,10 +142,12 @@ int main() {
         SDL_RenderFillRect(ren, &coin);
 
         // Obstáculos
-        SDL_SetRenderDrawColor(ren, 180, 0, 0, 255);
-        for (int i = 0; i < NUM_OBS; i++) {
-            SDL_Rect r = {j.obstaculos[i].x * TILE, j.obstaculos[i].y * TILE, TILE, TILE};
-            SDL_RenderFillRect(ren, &r);
+        if (mostrarObstaculos) {
+            SDL_SetRenderDrawColor(ren, 180, 0, 0, 255);
+            for (int i = 0; i < NUM_OBS; i++) {
+                SDL_Rect r = {j.obstaculos[i].x * TILE, j.obstaculos[i].y * TILE, TILE, TILE};
+                SDL_RenderFillRect(ren, &r);
+            }
         }
 
         // Jugador
@@ -140,8 +163,6 @@ int main() {
 
         SDL_RenderPresent(ren);
 
-        // Control de FPS (~60)
-        Uint32 now = SDL_GetTicks();
         if (now - lastTime < 16)
             SDL_Delay(16 - (now - lastTime));
         lastTime = now;
@@ -150,21 +171,21 @@ int main() {
     // Game Over
     SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
     SDL_RenderClear(ren);
-
+    
     SDL_Color red = {255, 50, 50, 255};
     SDL_Color white = {255, 255, 255, 255};
-
+    
     render_text(ren, font, "GAME OVER", WIDTH / 2 - 80, HEIGHT / 2 - 40, red);
-
+    
     char final[64];
     sprintf(final, "Puntaje final: %d", j.puntaje);
     render_text(ren, font, final, WIDTH / 2 - 90, HEIGHT / 2 + 10, white);
-
+    
     render_text(ren, font, "Presiona cualquier tecla para salir", WIDTH / 2 - 190, HEIGHT / 2 + 60, white);
-
+    
     SDL_RenderPresent(ren);
 
-    // Cierre
+	// Cierre
     int esperar = 1;
     while (esperar) {
         while (SDL_PollEvent(&e)) {
@@ -173,8 +194,8 @@ int main() {
         }
         SDL_Delay(10);
     }
-
-    // Limpieza
+    
+	// Limpieza
     TTF_CloseFont(font);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
